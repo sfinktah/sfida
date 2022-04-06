87b26b344dbb81058fea718dbc7b0cb4a8b24f1a
Sfinktah Bungholio <sfinktah@github.spamtrak.org>
2022-04-07 06:34:08 +1000

    added

---
+++ process_natives.py
@@ -0,0 +1,6 @@
+from execfile import execfile, make_refresh
+refresh_natives = make_refresh(os.path.abspath(__file__))
+
+l = [x[2] + 0x140000000 for x in natives if x[2] and x[2] != 0xffffffff]
+m = [x[3] + 0x140000000 for x in natives if x[3] and x[3] != 0xffffffff and x[3] + 0x140000000 not in l]
+
