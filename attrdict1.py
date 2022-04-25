e68d54374fb31c775eaa6b429c82f0545794de88
Sfinktah Bungholio <sfinktah@github.spamtrak.org>
2022-04-25 19:32:20 +1000

    merged attrdict0 and 1

--- attrdict1.py
+++ attrdict1.py
@@ -2,3 +2,5 @@ class SimpleAttrDict(dict):
     def __init__(self, *args, **kwargs):
         super(SimpleAttrDict, self).__init__(*args, **kwargs)
         self.__dict__ = self
+
+AttrDict = SimpleAttrDict
